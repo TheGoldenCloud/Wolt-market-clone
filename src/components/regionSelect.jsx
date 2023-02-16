bcc3915dec461da7b89300b621fe139915da1cb3
GoldenCloudPc <jvladimir54@yahoo.com>
2023-02-16 23:41:52 +0100

    Added styles to select and input and added icon

--- src/components/regionSelect.jsx
+++ src/components/regionSelect.jsx
@@ -1,29 +1,35 @@
 import React from 'react'
 import "bootstrap/dist/css/bootstrap.css";
+import style from '../styles/style.module.css'
+import CloseIcon from '@mui/icons-material/Close';
+import { IconButton } from '@mui/material';
+import { Box } from "@mui/system";
+
 import region from '../photos/region.png'
 
 function RegionSelect() {
 
-  
-
-  let style = {
-    main:{
-        width:"300px",
-        border:"1px solid black",
-        padding: "5px"
-    }
-
-  }
-
   return (
     <React.Fragment>
-        <div style={style.main}>
-            <h3>Dodaj adresu</h3>
-            <select >
-                <option></option>
+        <div className = {style.adrWindow}>
+          
+          <div className = {style.closeBtn}>
+            <Box>
+              <IconButton>
+                <CloseIcon fontSize='medium' />
+              </IconButton>
+            </Box>
+          </div>
+
+            <h3>Add address</h3>
+            <select placeholder='Select state' className= {style.selectStreet} >
+                <option>Select state</option>
+                <option>Hello</option>
+                <option>Hello</option>
+                <option>Hello</option>
             </select>
             <br/>
-            <input />
+            <input className = {style.inputState} placeholder='State'/>
         </div>
     </React.Fragment>
   )
