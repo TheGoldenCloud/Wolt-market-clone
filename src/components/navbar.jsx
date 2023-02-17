3a8e31c74db35c2c3c53edcbb4b0d3637f15846d
GoldenCloudPc <jvladimir54@yahoo.com>
2023-02-17 20:38:10 +0100

    Moved styles to css file

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,35 +1,19 @@
 import React from 'react'
-import "bootstrap/dist/css/bootstrap.css";
+import style from '../styles/style.module.css'
 import wolticon from '../photos/wolticon.png';
 
 function Navbar() {
 
-  //Prebaci stylove u css fajl
-  let style = {
-    navbar:{
-      paddingTop: "18px",
-      display: "flex",
-      justifyContent: "space-between"
-    },
-
-    inputFeald:{
-      backgroundColor:"whitesmoke",
-      
-    }
-
-  }
-
   return (
     <React.Fragment>
-    <div style={style.navbar}>
-
-
+    <div className = {style.navbar}>
+    
       <div className='leftNav'>
         <img src={wolticon} width = '80'/>
       </div>
 
       <div className='centerNav'>
-        <input></input>
+        <input className={style.inputFeald}></input>
       </div>
 
       <div className='rightNav'>
