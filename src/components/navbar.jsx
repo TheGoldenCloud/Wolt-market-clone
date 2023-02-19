45a109d51ba7eb122e8feeb9219630578e6befd5
GoldenCloudPc <jvladimir54@yahoo.com>
2023-02-19 21:29:58 +0100

    Added left navbar to select region

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import style from '../styles/style.module.css'
 import wolticon from '../photos/wolticon.png';
+import RegionNavSelect from './regionNavSelect'
 
 function Navbar() {
 
@@ -8,8 +9,10 @@ function Navbar() {
     <React.Fragment>
     <div className = {style.navbar}>
     
-      <div className='leftNav'>
-        <img src={wolticon} width = '80'/>
+      <div className= {style.leftNav}>
+        {/* <img src={wolticon} className = {style.navImg} width = '70' height='30'/> */}
+        <img src={wolticon} className = {style.navImg}/>
+        <RegionNavSelect/>
       </div>
 
       <div className='centerNav'>
@@ -17,8 +20,24 @@ function Navbar() {
       </div>
 
       <div className='rightNav'>
-        <a href='#'>Prijava</a>
-        <button className='btn btn-primary' >Registracija</button>
+        
+        <div className="btn-group">
+          <button type="button" className="btn btn-secondary"> 
+            <div className="profile">
+              pr
+            </div>
+          </button>
+          <button type="button" className="btn btn-secondary dropdown-toggle dropdown-toggle-split" id="dropdownMenuReference" data-bs-toggle="dropdown" aria-expanded="false" data-bs-reference="parent">
+            <span className="visually-hidden">Toggle Dropdown</span>
+          </button>
+          <ul className="dropdown-menu" aria-labelledby="dropdownMenuReference">
+            <li><a className="dropdown-item" href="#">Prijava ili registracija</a></li>
+            <li><hr className="dropdown-divider" /></li>
+            <li><a className="dropdown-item" href="#">Jezik: Srpski</a></li>
+            <li><a className="dropdown-item" href="#">Podrska</a></li>
+            
+          </ul>
+        </div>
       </div>
 
       
